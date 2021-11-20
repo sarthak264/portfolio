dac8d84cd4f859308ad2376b954c6d4a4330e5bd
sarthak264 <sarthaksharm372@gmail.com>
2021-11-20 12:36:11 +0530

    built the smaller projects section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Header from "./components/Header";
+import OtherProject from "./components/OtherProject";
 
 function App() {
   return (
     <div className="page">
       <Header />
       <About />
+      <OtherProject />
       <Contact />
       <Footer />
     </div>
---
+++ src/utils/OtherProjects.js
@@ -0,0 +1,52 @@
+const OtherProjects = [
+  {
+    title: "Landing Page",
+    description:
+      "A dark themed landing page of a hypothetical application made with ReactJS with slider and tabs layout made for Practise.",
+    technologies: ["ReactJS", "Bootstrap", "CSS"],
+    github: "https://github.com/sarthak264/landing-page",
+    link: "https://landing-page-new001.netlify.app/",
+  },
+  {
+    title: "Landing Page",
+    description:
+      "A dark themed landing page of a hypothetical application made with ReactJS with slider and tabs layout made for Practise.",
+    technologies: ["ReactJS", "Bootstrap", "CSS"],
+    github: "https://github.com/sarthak264/landing-page",
+    link: "https://landing-page-new001.netlify.app/",
+  },
+  {
+    title: "Landing Page",
+    description:
+      "A dark themed landing page of a hypothetical application made with ReactJS with slider and tabs layout made for Practise.",
+    technologies: ["ReactJS", "Bootstrap", "CSS"],
+    github: "https://github.com/sarthak264/landing-page",
+    link: "https://landing-page-new001.netlify.app/",
+  },
+  {
+    title: "Landing Page",
+    description:
+      "A dark themed landing page of a hypothetical application made with ReactJS with slider and tabs layout made for Practise.",
+    technologies: ["ReactJS", "Bootstrap", "CSS"],
+    github: "https://github.com/sarthak264/landing-page",
+    link: "https://landing-page-new001.netlify.app/",
+  },
+  {
+    title: "Landing Page",
+    description:
+      "A dark themed landing page of a hypothetical application made with ReactJS with slider and tabs layout made for Practise.",
+    technologies: ["ReactJS", "Bootstrap", "CSS"],
+    github: "https://github.com/sarthak264/landing-page",
+    link: "https://landing-page-new001.netlify.app/",
+  },
+  {
+    title: "Landing Page",
+    description:
+      "A dark themed landing page of a hypothetical application made with ReactJS with slider and tabs layout made for Practise.",
+    technologies: ["ReactJS", "Bootstrap", "CSS"],
+    github: "https://github.com/sarthak264/landing-page",
+    link: "https://landing-page-new001.netlify.app/",
+  },
+];
+
+export default OtherProjects;
